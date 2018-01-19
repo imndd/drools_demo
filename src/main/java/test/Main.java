61c9ad2050977803cd320c8c73f8931062cba931
jiangjun <jiang.jun@live.com>
2018-01-19 18:50:02 +0800

    Initial commit

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class Main {
+    public static void main(String[] args) {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession("test-session");
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
+}
